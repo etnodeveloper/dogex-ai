adc8ec79d5a238073617c763affe5c147313c3ae
etnodeveloper <etno.developer@gmail.com>
2024-12-20 01:12:31 +0200

    Initial setup 👾

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import React from "react";
+import { Twitter } from "lucide-react";
+
+import Particles from "./components/particles";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+      <nav className="my-10 animate-fade-in">
+        <Link href='https://twitter.com/dogeX_ai' className="relative cursor-pointer z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+          <Twitter size={20} />
+        </Link>
+      </nav>
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <Particles
+        className="absolute inset-0 -z-10 animate-fade-in"
+        quantity={100}
+      />
+      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+        DogeX AI
+      </h1>
+
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="my-16 text-center animate-fade-in">
+        <h2 className="text-sm text-zinc-500 ">
+          Woof woof
+        </h2>
+      </div>
+    </div>
+  );
+}
